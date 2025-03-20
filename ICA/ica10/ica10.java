79972d05e69072558135f1014af4353fcf9c0bce
GideonHarris <166659190+GideonHarris@users.noreply.github.com>
2025-03-20 10:36:11 -0600

    a poor attempt at ica10

---
+++ ICA/ica10/ica10.java
@@ -0,0 +1,16 @@
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+String link = "<a href=\"http://abcd.efg.com\" target=\"_blank\">http://abcd.efg.com</a>";
+
+const name = ica10html.querySelector(".button");
+
+function slideCard(){
+    var button = ica10htmlt.getElementById("Acting");
+    button.slideCard = "animate";
+}
+
