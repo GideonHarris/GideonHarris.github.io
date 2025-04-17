32d05956ecdfac951bbb8ed538115b6675496418
GideonHarris <166659190+GideonHarris@users.noreply.github.com>
2025-04-16 23:02:08 -0600

    i forgor to get rid f this

--- ICA/ica13/main.js
+++ ICA/ica13/main.js
@@ -64,7 +64,7 @@ class Ball {
             const distance = Math.sqrt(dx * dx + dy * dy);
     
             if (distance < this.size + ball.size) {
-              ball.color = this.color = randomRGB();
+              
               this.velX = -this.velX;
               this.velY = -this.velY;
             }
