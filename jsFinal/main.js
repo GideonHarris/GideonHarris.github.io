4f7596c9d19068e9a4b59303d12f2926f0fde2a4
GideonHarris <166659190+GideonHarris@users.noreply.github.com>
2025-04-24 14:54:18 -0600

    startingJS

---
+++ jsFinal/main.js
@@ -0,0 +1,14 @@
+const buttonVar = document.querySelector("inputButton");
+buttonVar.addEventListener("click", function(){
+    thatsNotYourNumber(count);
+});
+
+const thingsToSay = ["lol no shot thats your number, please try again", "nah, I don't believe you", "Just type it in again", "wait hold on I forgot try again", "What?", "wait, lemme grab a pen, enter it again", "Wait I swear that is my number, try again", "I think you're lying", "Eat shit, try agian"];
+let count = 0;
+
+
+function thatsNotYourNumber(Count){
+    if (Count === 0){
+        
+    };
+};
