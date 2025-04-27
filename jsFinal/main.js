d17253b6e061de314b42ada44c5d3481ff07207e
GideonHarris <166659190+GideonHarris@users.noreply.github.com>
2025-04-27 08:47:14 -0600

    just fuckin around on final

--- jsFinal/main.js
+++ jsFinal/main.js
@@ -1,14 +1,39 @@
+// grabbing the things we need from our html 
 const buttonVar = document.querySelector("inputButton");
+var message = document.getElementById("message");
+
+//declaring all of our variables we will use
+let count = 0;
+let goal = randomNum();
+
+
 buttonVar.addEventListener("click", function(){
-    thatsNotYourNumber(count);
+    thatsNotYourNumber(count, goal);
 });
 
 const thingsToSay = ["lol no shot thats your number, please try again", "nah, I don't believe you", "Just type it in again", "wait hold on I forgot try again", "What?", "wait, lemme grab a pen, enter it again", "Wait I swear that is my number, try again", "I think you're lying", "Eat shit, try agian"];
-let count = 0;
 
 
-function thatsNotYourNumber(Count){
+//our funcitons to randomly generate how many times they are going to have to enter in their number 
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  function randomNum() {
+    return random(0, 50);
+    // 50 because I am cruel
+  };
+
+function thatsNotYourNumber(Count, Goal){
+    let ramdomMessage = thingsToSay[random(0, thingsToSay.length - 1)];
     if (Count === 0){
         
+    }
+    else if(Count === Goal){
+
+    }
+    else{
+
     };
 };
