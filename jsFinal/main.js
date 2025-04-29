056d1631fb11d7b1ebbb5b6aa1d4b514e75de247
GideonHarris <166659190+GideonHarris@users.noreply.github.com>
2025-04-29 10:43:05 -0600

    Meow

--- jsFinal/main.js
+++ jsFinal/main.js
@@ -15,7 +15,7 @@ buttonVar.addEventListener("click", function(){
     count++;
 });
 
-const thingsToSay = ["lol no shot thats your number, please try again", "Wait no I think that's my number, try again","nah, I don't believe you", "Just type it in again", "wait hold on I forgot, try again", "What? Try again", "wait, lemme grab a pen, enter it again", "Wait I swear that is my number, try again", "I think you're lying, please enter your REAL number", "Eat shit, try agian"];
+const thingsToSay = ["lol no shot thats your number, please try again", "Wait no I think that's my number, try again","nah, I don't believe you", "Just type it in again", "wait hold on I forgot, try again", "What? Try again", "wait, lemme grab a pen, enter it again", "Wait I swear that is my number, try again", "I think you're lying, please enter your REAL number", "Eat shit, try agian", "Meow. Try again."];
 
 
 //our funcitons to randomly generate how many times they are going to have to enter in their number 
